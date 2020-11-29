9e4a2e462cd620bd11bacc4509c88bacb3ccb589
asafraph <74338785+asafraph@users.noreply.github.com>
2020-11-29 16:09:40 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const { default: uuid, v4 } = require("uuid")
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'vacations',
+	host: 'us-mm-auto-sfo-02-bh.cleardb.net',
+	user: 'bb0f1923690c75',
+	password: '5792799d',
+	database: 'heroku_6c24fc72a58f31d',
 })
 
 con.connect( (err)=> {
